75a0b7017275b02419721c7a921108fe36dd1916
Ederson Silva <edersigner@gmail.com>
2023-02-20 10:44:59 -0300

    Initial commit

---
+++ src/test/java/com/codevops/javagraphql/JavaGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codevops.javagraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
